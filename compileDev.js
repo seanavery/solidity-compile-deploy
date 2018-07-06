4062f4218f8ba3fcf8261fbbc95a80999a96011e
Sean Pollock <seanpollock3344@gmail.com>
2018-07-06 19:41:21 -0400

    fixing solc compilation

--- compileDev.js
+++ compileDev.js
@@ -1,14 +1,20 @@
 import { promises as fs } from 'fs'
+import solc from 'solc'
 
 export const compile = async () => {
     if (typeof process.argv[2] === 'undefined')
         throw new Error('please provide contract directory path')
     const contracts = await getContractFiles(process.argv[2])
-    const contractData = await Promise.all(
+
+    const contractsData = {}
+    await Promise.all(
         contracts.map(async contract => {
-            return await getContractData(process.argv[2], contract)
+            const contractData = await getContractData(process.argv[2], contract)
+            contractsData[contract] = contractData
         })
     )
+
+    const compiledData = await compileContracts(contractsData)
 }
 
 const getContractFiles = async (contractPath) => {
@@ -32,6 +38,15 @@ const getContractData = async (contractPath, contract) => {
         return data.toString()
     } catch (err) {
         throw new Error('could not read contract file')
+    }
+}
+
+const compileContracts = async (contractsData) => {
+    try {
+        const compiledData = await solc.compile({sources: contractsData}, 1)
+        return compiledData
+    } catch (err) {
+        throw new Error('could not compile contract', err)
     }
 }
 
