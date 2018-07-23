38619d1801d76662410fe72ce2493ea55fe2712f
Sean Pollock <seanpollock3344@gmail.com>
2018-07-23 11:15:54 -0400

    removing old code

--- deploy.js
+++ deploy.js
@@ -1,45 +1,51 @@
-import Promise from 'bluebird';
-import Eth from 'ethjs';
+import { promises as fs } from 'fs'
+import { providers } from 'ethers'
+import { config } from './deploy.config.js'
 
-const fs = Promise.promisifyAll(require('fs'));
-const jsonfile = Promise.promisifyAll(require('jsonfile'));
+const deployContracts = async () => {
+    const contractsPath = process.argv[2]
+    if (typeof contractsPath === 'undefined')
+        throw new Error('please provide contract directory path')
 
-let HttpProvider;
-let eth;
+    const compiledData = await readCompiledData(contractsPath)
 
-export function deploy() {
-  return new Promise((resolve, reject) => {
-    Promise.resolve(web3Provider())
-    .then(() => {
-      getCompiled();
-    }).catch((error) => {
-      reject(error);
-    });
-  });
+    const provider = initProvider()
+
+    console.log('provider', provider)
+
+    // await Promise.all(
+    //     Object.keys(compiledData.contracts)
+    //         .map(async contract => {
+    //             await deployContract(compiledData.contracts[contract])}))
+
+    await Promise.all(
+        config.contracts.map(async contract => {
+            await deployContract(contract)
+        })
+    )
 }
 
-export function web3Provider() {
-  return new Promise((resolve, reject) => {
-    Promise.delay(0)
-    .then(() => {
-      eth = new Eth(new Eth.HttpProvider('http://localhost:8545'));
-      resolve(true);
-    }).catch((error) => {
-      reject(error);
-    })
-  });
+const readCompiledData = async (contractsPath) => {
+    try {
+        const data = await fs.readFile(`${process.cwd()}/${contractsPath}/compiled.json`)
+        return JSON.parse(data.toString('utf8'))
+    } catch (err) {
+        throw new Error('could not read compiled data', err)
+    }
 }
 
-export function getCompiled() {
-  return new Promise((resolve, reject) => {
-    jsonfile.readFileAsync(`${process.cwd()}/EVM/compiled/compiled.json`)
-    .then((data) => {
-      console.log('data', data);
-      resolve(true);
-    }).catch((error) => {
-      reject(error);
-    });
-  });
+const deployContract = async (contract) => {
+    try {
+
+    } catch {
+        throw new Error(`error deployin contract ${contract}`, err)
+    }
 }
 
-deploy();
+const initProvider = () => {
+    const network = providers.networks.ropsten
+    return new providers.InfuraProvider(network)
+}
+
+deployContracts()
+.catch(err => console.log('error deploying contracts', err))
